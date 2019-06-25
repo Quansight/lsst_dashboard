3c7317e783bd321948a1ccaae5e4f99488897353
Tony Fast <tony.fast@gmail.com>
2019-06-25 06:04:41 +0000

    Add the project layout

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+__name__ == "__main__" and setuptools.setup(**dict(
+    name="lsst_dashbard",
+    version='0.0.1',
+    author="quansight",
+    author_email="tony.fast@quansight.com",
+    description="Conventions for writing code in the notebook.",
+    long_description_content_type='text/markdown',
+    url="https://github.com/quanisght/lsst_dashboard",
+    python_requires=">=3.6",
+    license="BSD-3-Clause",
+    install_requires="vdom importnb holoviews param pandas ibis scikit-learn".split(),
+    include_package_data=True,
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'':'src',},
+    entry_points = {'pytest11': [],})
+)
+
+"""A minimal "setup.py" file for the `lsst_dashboard` project then include notebooks in the source with `importnb`; we'll mature into python scripts as the tooling stabilizes.""";
---
+++ src/lsst_dashboard/__init__.py
@@ -0,0 +1,2 @@
+with __import__('importnb').Notebook():
+    from . import overview, data, details, star_map, visits, psfmag, flags, utils
