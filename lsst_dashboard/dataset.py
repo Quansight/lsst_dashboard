02d6cfe5a5be31620689624487426aba23142e77
Dharhas Pothina <dharhas@quansight.com>
2019-07-22 09:28:40 -0500

    initial version of Dataset class

---
+++ lsst_dashboard/dataset.py
@@ -0,0 +1,88 @@
+import yaml
+from pathlib import Path
+
+try:
+    from lsst.daf.persistence import Butler
+except ImportError:
+    Butler = None
+
+METADATA_FILENAME = 'metadata.yaml'
+
+def _default_metrics():
+    return [
+        'base_Footprint_nPix', 'Gaussian-PSF_magDiff_mmag', 'CircAper12pix-PSF_magDiff_mmag', 
+        'Kron-PSF_magDiff_mmag', 'CModel-PSF_magDiff_mmag', 'traceSdss_pixel', 'traceSdss_fwhm_pixel', 
+        'psfTraceSdssDiff_percent', 'e1ResidsSdss_milli', 'e2ResidsSdss_milli', 'deconvMoments', 
+        'compareUnforced_Gaussian_magDiff_mmag', 'compareUnforced_CircAper12pix_magDiff_mmag', 
+        'compareUnforced_Kron_magDiff_mmag', 'compareUnforced_CModel_magDiff_mmag'
+    ]
+
+def _default_flags():
+    return [
+        'calib_psf_used', 'calib_psf_candidate', 'calib_photometry_reserved', 'merge_measurement_i2', 
+        'merge_measurement_i', 'merge_measurement_r2', 'merge_measurement_r', 'merge_measurement_z', 
+        'merge_measurement_y', 'merge_measurement_g', 'merge_measurement_N921', 'merge_measurement_N816', 
+        'merge_measurement_N1010', 'merge_measurement_N387', 'merge_measurement_N515', 'qaBad_flag'
+    ]
+
+class Dataset():
+    def __init__(self, path):
+        self.path = Path(path)
+        self.metadata_path = self.path.joinpath(METADATA_FILENAME)
+        self.conn = None
+        self.metadata = None
+        self.tables = {}
+    
+    def connect(self):
+        if not self.path.joinpath('metadata.yaml').exists(): # todo: remove this once we have metadata yaml files saved in the data folders
+            self.metadata = {'metrics': _default_metrics(), 'flags': _default_flags()}
+        else:
+            with self.metadata.open('r') as f:
+                self.metadata = json.load(f)
+
+        # if Butler is available use it to connect. If not available we are reading from disk
+        if Butler: # 
+            self.conn = Butler(str(self.path))
+
+    def get_table(self, table, tract, filter):
+        if self.conn:
+            return self.conn.get(table, tract=tract, filter=filter)
+        else:
+            return pd.read_parquet(self.path.joinpath(f'{table}_{tract}_{filter}.parq'))
+
+    def parse_tables(self, tables=None, tracts=None, filters=None, metrics=None):
+
+        if tables is None:
+            tables = self.metadata['tables']
+
+        if tracts is None:
+            tracts = self.metadata['tracts']
+
+        if filters is None:
+            filters = self.metadata['filters']
+
+        if metrics is None:
+            metrics = self.metadata['metrics']
+
+        dataset = {}
+        for filt in filters:
+            dataset[filt] = {}
+            for table in tables:
+                dataset[filt][table] = {}
+                for tract in tracts:
+                    dataset[filt][table][tract] = self.get_table(table, tract, filt)
+                    #if 'coord' in table:
+                    #    dataset[filt][table][tract] = dataset[filt][table][tract].toDataFrame(columns=metrics)
+                    #else:
+                    #    dataset[filt][table][tract] = dataset[filt][table][tract].toDataFrame()
+
+        self.tables = dataset
+
+
+#def open_dataset(path):
+#    
+#    p = Path(path)
+#    if p.exists():
+#        dataset = Dataset(path)
+#        dataset.connect()
+        
