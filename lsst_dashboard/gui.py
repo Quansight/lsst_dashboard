25365096ef7ecf4cd5895bd784c146fa17a8a7c7
brendancol <parietal.io@gmail.com>
2019-07-24 08:38:34 -0500

    add mock plot for testing

--- lsst_dashboard/gui.py
+++ lsst_dashboard/gui.py
@@ -161,5 +161,32 @@ class DetailComponent(Component):
         return self.overview._selected_info
 
 
+def mock_plot():
+
+    from bokeh.models import ColumnDataSource
+    from bokeh.palettes import Spectral6
+    from bokeh.plotting import figure
+    from bokeh.transform import factor_cmap
+
+    fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+    counts = [5, 3, 4, 2, 4, 6]
+
+    source = ColumnDataSource(data=dict(fruits=fruits, counts=counts))
+
+    p = figure(x_range=fruits, plot_height=350, toolbar_location=None, title="Fruit Counts")
+    p.vbar(x='fruits', top='counts', width=0.9, source=source, legend="fruits",
+           line_color='white', fill_color=factor_cmap('fruits', palette=Spectral6, factors=fruits))
+
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+    p.y_range.end = 9
+    p.legend.orientation = "horizontal"
+    p.legend.location = "top_center"
+
+    return p
+
+
+import holoviews as hv
+hv.extension('bokeh')
 pn.extension()
 dashboard = Application(body=TabComponent(QuickLookComponent()))
