f7c2a8e40959d7b7bc54e834e80b71dfe3c06cfb
Dharhas Pothina <dharhas@quansight.com>
2019-08-08 00:48:26 -0500

    refactor Dataset class to use dask

--- lsst_dashboard/dataset.py
+++ lsst_dashboard/dataset.py
@@ -1,11 +1,15 @@
 import yaml
 from pathlib import Path
+import dask.dataframe as dd
+from dask import delayed
 import pandas as pd
+import os
+import itertools
 
 try:
     from lsst.daf.persistence import Butler
     from lsst.qa.explorer.functors import StarGalaxyLabeller, Magnitude, RAColumn, DecColumn, CompositeFunctor
-    funcs = CompositeFunctor({'label': StarGalaxyLabeller(), 
+    transforms = CompositeFunctor({'label': StarGalaxyLabeller(), 
                           'psfMag': Magnitude('base_PsfFlux_instFlux'),
                           'ra': RAColumn(),
                           'dec': DecColumn()})
@@ -13,45 +17,41 @@ except ImportError:
     Butler = None
     StarGalaxyLabeller, Magnitude, RAColumn, DecColumn, CompositeFunctor = [None, None, None, None, None]
 
+
 METADATA_FILENAME = 'metadata.yaml'
 
-def _default_metrics():
-    return [
-        'base_Footprint_nPix', 'Gaussian-PSF_magDiff_mmag', 'CircAper12pix-PSF_magDiff_mmag', 
-        'Kron-PSF_magDiff_mmag', 'CModel-PSF_magDiff_mmag', 'traceSdss_pixel', 'traceSdss_fwhm_pixel', 
-        'psfTraceSdssDiff_percent', 'e1ResidsSdss_milli', 'e2ResidsSdss_milli', 'deconvMoments', 
-        'compareUnforced_Gaussian_magDiff_mmag', 'compareUnforced_CircAper12pix_magDiff_mmag', 
-        'compareUnforced_Kron_magDiff_mmag', 'compareUnforced_CModel_magDiff_mmag'
-    ]
-
-def _default_flags():
-    return [
-        'calib_psf_used', 'calib_psf_candidate', 'calib_photometry_reserved', 'merge_measurement_i2', 
-        'merge_measurement_i', 'merge_measurement_r2', 'merge_measurement_r', 'merge_measurement_z', 
-        'merge_measurement_y', 'merge_measurement_g', 'merge_measurement_N921', 'merge_measurement_N816', 
-        'merge_measurement_N1010', 'merge_measurement_N387', 'merge_measurement_N515', 'qaBad_flag'
-    ]
-
 class Dataset():
+    """
+    USAGE:
+        d = Dataset(path)
+        d.connect()
+        d.init_data()
+    """
     def __init__(self, path):
         self.path = Path(path)
-        self.metadata_path = self.path.joinpath(METADATA_FILENAME)
-        self.conn = None
-        self.metadata = None
         self.tables = {}
+        self.visits = {}
     
     def connect(self):
-        if not self.path.joinpath('metadata.yaml').exists(): # todo: remove this once we have metadata yaml files saved in the data folders
-            self.metadata = {'metrics': _default_metrics(), 'flags': _default_flags()}
-        else:
-            with self.metadata.open('r') as f:
-                self.metadata = json.load(f)
+        # search for metadata file
+        if self.path.joinpath(METADATA_FILENAME).exists():
+            self.metadata_path = self.path.joinpath(METADATA_FILENAME)
+        else: 
+            self.metadata_path = Path(os.environ.get('LSST_META', os.curdir)).joinpath(self.path.name + f'_{METADATA_FILENAME}')
+
+        with self.metadata_path.open('r') as f:
+            self.metadata = yaml.load(f, Loader=yaml.SafeLoader)
+            self.filters = self.metadata.get('filters')
+            self.metrics = self.metadata.get('metrics')
+            self.failures = self.metadata.get('failures')
+            self.flags = self.metadata.get('flags')
+            self.tracts = self.metadata.get('tracts')
 
         # if Butler is available use it to connect. If not available we are reading from disk
         if Butler: # 
             self.conn = Butler(str(self.path))
 
-    def get_table(self, table, tract, filt, sample=None):
+    def get_table(self, table, tract, filt):
         if self.conn:
             df = self.conn.get(table, tract=int(tract), filter=filt)
         else: 
@@ -59,61 +59,48 @@ class Dataset():
 
         return df
 
-    def fetch_tables(self, tables=None, tracts=None, filters=None, metrics=None, flags=None, sample=None):
+    def init_data(self):
+        print('loading coadd_forced table')
+        self.fetch_coadd_table('forced')
+        print('loading coadd_unforced table')
+        self.fetch_coadd_table('unforced')
+        for filt in self.filters:
+            print(f'loading visit data for filter {filt}')
+            self.fetch_visits(filt)
 
-        if tables is None:
-            tables = self.metadata['tables']
+    def fetch_coadd_table(self, version='unforced'):
+        table = 'analysisCoaddTable_' + version
+        dfs = []
+        for filt in self.filters:
+            for tract in self.tracts:
+                dfs.append(delayed(self._load_coadd_table)(table, filt, tract))
+        
+        self.tables[table] = dd.from_delayed(dfs)
 
-        if tracts is None:
-            tracts = self.metadata['tracts']
+    def fetch_visits(self, filt):
+        visits = []
+        for tract in self.tracts:
+            df = self.get_table('visitMatchTable', tract, filt).toDataFrame()
+            visits.append([delayed(self._fetch_visit)(visit, tract, filt) for visit in df['matchId'].columns])
+                    
+        self.visits[filt] = dd.from_delayed(list(itertools.chain(*visits)))
 
-        if filters is None:
-            filters = self.metadata['filters']
-
-        if metrics is None:
-            metrics = self.metadata['metrics']
-
-        if flags is None:
-            flags = self.metadata['flags']
-
-        dataset = {}
-        for filt in filters:
-            dataset[filt] = {}
-            for table in tables:
-                dataset[filt][table] = {}
-                for tract in tracts:
-                    print(f'filt={filt}, table={table}, tract={tract}')
-                    dataset[filt][table][tract] = self.get_table(table, tract, filt, sample)
-                    if 'coadd' in table.lower():
-                        df = dataset[filt][table][tract]
-                        new_cols = funcs(df)
-                        cols = metrics + flags + ['patchId', 'id']
-                        df = pd.concat([df.toDataFrame(columns=cols), new_cols], axis=1)
-                    else:
-                        df = dataset[filt][table][tract].toDataFrame()
-
-                    if sample:
-                        df = df.sample(sample)
-                    dataset[filt][table][tract] = df
-
-        self.tables = dataset
-
-    def fetch_visit(self, visit, tract, filt, sample=None):
-        df = self.conn.get('analysisVisitTable', visit=int(visit), tract=int(tract), filter=filt).toDataFrame(self.metadata['metrics'])
-        if sample:
-            df = df.sample(sample)
+    def _load_coadd_table(self, table, filt, tract):
+        df = self.get_table(table, tract, filt)
+        new_cols = transforms(df)
+        cols = self.metrics + self.flags + ['patchId', 'id']
+        df = pd.concat([df.toDataFrame(columns=cols), new_cols], axis=1)
+        df['filter'] = filt
+        df['tract'] = tract
         return df
 
-    def fetch_visits(self, tracts, filters, sample=None):
-        self.visits = {}
-        for filt in filters:
-            self.visits[filt] = {}
-            for tract in tracts:
-                print(f'filt={filt}, tract={tract}')
-                visits = pd.concat({str(visit): self.fetch_visit(visit, tract, filt, sample) for visit in self.tables[filt]['visitMatchTable'][tract]['matchId'].columns.astype(str)})
-                # leave this transform for later so we can save a simpler file
-                # visits = visits.set_index(pd.MultiIndex.from_arrays([pd.CategoricalIndex(visits.index.get_level_values(0).astype(str), name='visit'), visits.index.get_level_values('id')]))
-                self.visits[filt][tract] = visits
+    def _fetch_visit(self, visit, tract, filt):
+        df = self.conn.get('analysisVisitTable', visit=int(visit), tract=int(tract), filter=filt)
+        df = df.toDataFrame(self.metrics + self.flags)
+        df['visit'] = visit
+        df['tract'] = tract
+        df['filt'] = filt
+        return df
 
     def write_tables(self, path, filt, sample=None):
         p = Path(path)
@@ -151,17 +138,4 @@ class Dataset():
         self.tables = tables
         self.visits = visits
 
-
-#def open_dataset(path, filt, coadd_type=None, sample=None):
-#    d = Dataset(path)
-#    d.connect()
-#    if coadd_type is None:
-#        coadd_type = 'unforced'
-#
-#    tables = ['']
-#    if Butler:
-#        d.fetch_tables(tables='', tracts=tracts, filters=filters, sample=sample)
-#        d.fetch_visits(tracts=tracts, filters=filters, sample=1000)
-#    else:
-#        d.load_from_hdf(path)
         
