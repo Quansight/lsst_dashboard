7b4702a6b337a5097f35b57558c9a1d8e63a4e21
Timothy Morton <tim.morton@gmail.com>
2019-11-18 13:42:27 -0800

    change default data repo loc

--- lsst_dashboard/gui.py
+++ lsst_dashboard/gui.py
@@ -43,7 +43,7 @@ filtered_datasets = []
 datavisits = []
 filtered_datavisits = []
 
-sample_data_directory = '/project/tmorton/tickets/DM-21335/'
+sample_data_directory = '/project/tmorton/tickets/DM-21335/w38'
 
 class Store(object):
 
