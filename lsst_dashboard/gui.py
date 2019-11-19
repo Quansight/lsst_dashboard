75fcada8ebb31412ce107b398bc215b615f1845f
Timothy Morton <tim.morton@gmail.com>
2019-11-19 15:00:52 -0800

    actually comment out

--- lsst_dashboard/gui.py
+++ lsst_dashboard/gui.py
@@ -327,7 +327,7 @@ class QuickLookComponent(Component):
     def add_status_message(self, title, body, level='info', duration=5):
         msg = {'title': title, 'body': body}
         msg_args = dict(msg=msg, level=level, duration=duration)
-        self.status_message_queue.append(msg_args)  # to work with panel 0.7
+        # self.status_message_queue.append(msg_args)  # to work with panel 0.7
         self.param.trigger('status_message_queue')
         # Drop message in terminal/logger too
         try:
@@ -494,9 +494,9 @@ class QuickLookComponent(Component):
         self.status_message.object = '<ul style="{}">{}</ul>'.format(queue_css, html)
 
     def execute_js_script(self, js_body):
-        # script = '<script>(function() { ' + js_body +  '})()</script>'  # to work with panel 0.7
-        # self.adhoc_js.object = script  
-        self.adhoc_js.object = js_body
+        script = '<script>(function() { ' + js_body +  '})()</script>'  # to work with panel 0.7
+        self.adhoc_js.object = script  
+        # self.adhoc_js.object = js_body
 
     def get_patch_count(self):
         return 1
