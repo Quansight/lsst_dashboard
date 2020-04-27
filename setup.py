4c3b034abbdcd3f4a2de9d50f68bd4de33697640
Dharhas Pothina <dharhas@quansight.com>
2020-04-27 11:48:00 -0500

    rename __main__.py to cli.py

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ __name__ == "__main__" and setuptools.setup(
         name="lsst_dashboard",
         version="v0.0.2a",
         author="quansight",
-        author_email="tony.fast@quansight.com",
-        description="Conventions for writing code in the notebook.",
+        author_email="dharhas@quansight.com",
+        description="LSST Data Explorer",
         long_description_content_type="text/markdown",
-        url="https://github.com/quanisght/lsst_dashboard",
+        url="https://github.com/quansight/lsst_dashboard",
         python_requires=">=3.6",
         license="BSD-3-Clause",
-        install_requires=[],
+        install_requires=['click'],
         include_package_data=True,
         packages=setuptools.find_packages(),
         entry_points={
             "console_scripts": [
-                "lsst_data_explorer = lsst_dashboard.__main__:main",
-                "lsst_data_repartition = lsst_dashboard.__main__:repartition",
+                "lsst_data_explorer = lsst_dashboard.cli:cli",
             ]
         },
     )
